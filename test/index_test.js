ff0df3d55f5067b2230f42e52602e3d0b8ec34d5
PatrickAlphaC <collinspatrickkevin@gmail.com>
2020-10-08 18:38:54 -0400

    Revert "returns a fail, but posts the job at the moment"
    This reverts commit 7ffb625b1a77f8bd362e68b30ec2f8aebfd26041.

--- test/index_test.js
+++ test/index_test.js
@@ -1,27 +1,18 @@
 const assert = require('chai').assert
 const createRequest = require('../index.js').createRequest
-const URL = 'www.google.com'
 
 describe('createRequest', () => {
   const jobID = '1'
 
   context('successful calls', () => {
     const requests = [
-      {
-        name: 'id not supplied',
-        testData: { data: { url: URL } },
-      },
-      {
-        name: 'url',
-        testData: { id: jobID, data: { url: URL } },
-      },
-      {
-        name: 'url, endpoint',
-        testData: { id: jobID, data: { url: URL, endpoint: '/api/submit' } },
-      },
+      { name: 'id not supplied', testData: { data: { base: 'ETH', quote: 'USD' } } },
+      { name: 'base/quote', testData: { id: jobID, data: { base: 'ETH', quote: 'USD' } } },
+      { name: 'from/to', testData: { id: jobID, data: { from: 'ETH', to: 'USD' } } },
+      { name: 'coin/market', testData: { id: jobID, data: { coin: 'ETH', market: 'USD' } } }
     ]
 
-    requests.forEach((req) => {
+    requests.forEach(req => {
       it(`${req.name}`, (done) => {
         createRequest(req.testData, (statusCode, data) => {
           assert.equal(statusCode, 200)
@@ -39,10 +30,13 @@ describe('createRequest', () => {
     const requests = [
       { name: 'empty body', testData: {} },
       { name: 'empty data', testData: { data: {} } },
-      // TODO
+      { name: 'base not supplied', testData: { id: jobID, data: { quote: 'USD' } } },
+      { name: 'quote not supplied', testData: { id: jobID, data: { base: 'ETH' } } },
+      { name: 'unknown base', testData: { id: jobID, data: { base: 'not_real', quote: 'USD' } } },
+      { name: 'unknown quote', testData: { id: jobID, data: { base: 'ETH', quote: 'not_real' } } }
     ]
 
-    requests.forEach((req) => {
+    requests.forEach(req => {
       it(`${req.name}`, (done) => {
         createRequest(req.testData, (statusCode, data) => {
           assert.equal(statusCode, 500)
